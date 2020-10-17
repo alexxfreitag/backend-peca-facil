06d6b9368be70c66f2d72ac8a43993377dd097a9
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-10-17 16:29:20 -0300

    Configure code style with eslint, prettier and editorConfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import {Router} from 'express'
+import { Router } from 'express';
 
-const routes = new Router()
+const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ msg: 'Backend working'})
-})
+routes.get('/', (req, res) => res.json({ msg: 'Backend working' }));
 
-export default routes
+export default routes;
